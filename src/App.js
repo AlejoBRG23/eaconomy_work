3dbddea60fe94f71f7b82d7010444a993e3a270d
Alejandro <kenny282331@gmail.com>
2024-11-12 20:36:31 -0500

    mi trabajo en proposito

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/AlejoBRG23/Alejandro-Garcia"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ALEJANDRO_GARCIA
+        </a>
+        
+        <a
+          className="App-link"
+          href="https://manarafx.io/?token=c0v7rbp6jeiabgrds8qg&email=kenny282331@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MANARA
+        </a>
+        <a
+          className="App-link"
+          href="https://eaconomy.best/alejo0423/why-eaconomy"
+          target="_blank"
+          rel="noopener noreferrer"
+        > EACONOMY
+          
+        </a>
+        <a
+          className="App-link"
+          href="https://myaccount.infinox.com/home"
+          target="_blank"
+          rel="noopener noreferrer"
+        > INFINOX
+          
+        </a>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
