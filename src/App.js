d2f90dc3b96309f1e7e09585ea7d4636987fb287
Alejandro <kenny282331@gmail.com>
2024-11-19 20:01:19 -0500

    bsajdhgsadjh

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/AlejoBRG23/Alejandro-Garcia"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ALEJANDRO_GARCIA
-        </a>
-        
-        <a
-          className="App-link"
-          href="https://manarafx.io/?token=c0v7rbp6jeiabgrds8qg&email=kenny282331@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MANARA
-        </a>
-        <a
-          className="App-link"
-          href="https://eaconomy.best/alejo0423/why-eaconomy"
-          target="_blank"
-          rel="noopener noreferrer"
-        > EACONOMY
-          
-        </a>
-        <a
-          className="App-link"
-          href="https://myaccount.infinox.com/home"
-          target="_blank"
-          rel="noopener noreferrer"
-        > INFINOX
-          
-        </a>
+    <button>
+      I'm a button
+    </button>
+  );
+}
 
-        
-      </header>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
 }
 
-export default App;
